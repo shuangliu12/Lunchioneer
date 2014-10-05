780fa442489accf70815e0e8474702551c149d98
Shuang <shuangliu12@gmail.com>
2014-10-05 10:32:50 -0400

    create the users views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:show, :index, :edit, :update] do
 
+  end
   root "welcome#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
