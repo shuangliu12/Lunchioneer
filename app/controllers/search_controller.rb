8fce9cc718c6bcc446f02a86902c57dbea0ea204
Shuang <shuangliu12@gmail.com>
2014-10-16 23:33:27 -0400

    fix syntax error leads to heroku crash

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
-class SearchController < ActionController::Base
-  def index
-    @kitchens = Kitchen.where("city ilike ?", "%#{params[:q]}%")
-  #   @meals = Meal.where("meal_date ilike ?", "%#{params[:date_search]}%")
-  # end
-end
+# class SearchController < ActionController::Base
+#   def index
+#     @kitchens = Kitchen.where("city ilike ?", "%#{params[:q]}%")
+#   #   @meals = Meal.where("meal_date ilike ?", "%#{params[:date_search]}%")
+#   # end
+#   end
+# end
